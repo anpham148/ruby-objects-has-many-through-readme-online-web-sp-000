daaa330fa2bc3bd61421f3ba7445b7a98beb9214
An Pham <anpham148@gmail.com>
2020-07-17 15:27:04 +0000

    Automatically backed up by Learn

--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -214,6 +214,23 @@ describe "Object relationships" do
             expect(terrance.worst_tipper).to eq(josh)
       end
     end
-  
+    describe "#most_frequent_customer" do
+      it "returns the most frequent customer associated with this waiter" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        terrance = Waiter.new("Terrance", 1)
+        joe = Waiter.new("Joe", 10)
+        esmery = Waiter.new("Esmery", 2)
+        andrew = Waiter.new("Andrew", 3)
+
+        howard.new_meal(terrance, 15, 2)
+        howard.new_meal(joe, 15, 4)
+        howard.new_meal(andrew, 15, 5)
+        daniel.new_meal(terrance, 20, 1)
+        daniel.new_meal(esmery, 15, 3)
+
+
+      end
+    end
   end
 end
