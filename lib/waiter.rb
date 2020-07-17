ae03f5f6c5124035e9de3d353e1232e288f410f7
An Pham <anpham148@gmail.com>
2020-07-17 16:25:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -46,7 +46,7 @@ class Waiter
   def self.average_tip_for_most_experienced_waiter
     waiters_experience = Hash.new(0)
     self.all.each {|waiter| waiters_experience[waiter.name] = waiter.yrs_experience}
-    the_most_experienced_waiter = waiters_experience.sort_by{|name, yrs_experience| yrs_experience}.last[0]
+    the_most_experienced_waiter = waiters_experience.sort_by{|name, yrs_experience| yrs_experience}.last 
 
     
   end
